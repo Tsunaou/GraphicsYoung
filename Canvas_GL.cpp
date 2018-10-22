070de79f36e862f280e4160e6d820300ba69987c
Young <895254752@qq.com>
2018-10-22 13:50:33 +0800

    First commit for my graphics works

---
+++ Canvas_GL.cpp
@@ -0,0 +1,6 @@
+#include "Canvas_GL.h"
+
+Canvas_GL::Canvas_GL(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ Canvas_GL.h
@@ -0,0 +1,17 @@
+#ifndef CANVAS_GL_H
+#define CANVAS_GL_H
+
+#include <QWidget>
+
+class Canvas_GL : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Canvas_GL(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CANVAS_GL_H
